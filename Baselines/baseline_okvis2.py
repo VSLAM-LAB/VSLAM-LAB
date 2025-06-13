f0a089e181bc7c31d92dd08e10692a739f0faf4e
alejandrofontan <fontanvillacampa@gmail.com>
2025-06-13 13:18:07 +1000

    create conda package for okvis2

--- Baselines/baseline_okvis2.py
+++ Baselines/baseline_okvis2.py
@@ -1,4 +1,5 @@
 import os.path
+
 from path_constants import VSLAMLAB_BASELINES
 from Baselines.BaselineVSLAMLab import BaselineVSLAMLab
 
@@ -7,7 +8,8 @@ SCRIPT_LABEL = f"\033[95m[{os.path.basename(__file__)}]\033[0m "
 class OKVIS2_baseline(BaselineVSLAMLab):
     def __init__(self, baseline_name='okvis2', baseline_folder='OKVIS2'):
 
-        default_parameters = {'verbose': 1, 'mode': 'mono-vi'}
+        default_parameters = {'verbose': 1, 'mode': 'mono-vi', 
+                              'vocabulary': os.path.join(VSLAMLAB_BASELINES, baseline_folder, 'resources', 'small_voc.yml.gz')}
         
         # Initialize the baseline
         super().__init__(baseline_name, baseline_folder, default_parameters)
@@ -25,6 +27,6 @@ class OKVIS2_baseline_dev(OKVIS2_baseline):
         super().__init__(baseline_name = 'okvis2-dev', baseline_folder = 'OKVIS2-DEV')
 
     def is_installed(self):
-        is_installed = os.path.isfile(os.path.join(self.baseline_path, 'build', 'vslamlab_okvis2_mono_vi'))
+        is_installed = os.path.isfile(os.path.join(self.baseline_path, 'bin', 'vslamlab_okvis2_mono_vi'))
         return (True, 'is installed') if is_installed else (False, 'not installed (auto install available)') 
     
--- Baselines/get_baseline.py
+++ Baselines/get_baseline.py
@@ -1,43 +1,45 @@
 # ADD your imports here
-from Baselines.baseline_droidslam import DROIDSLAM_baseline
-from Baselines.baseline_droidslam import DROIDSLAM_baseline_dev
-from Baselines.baseline_mast3rslam import MAST3RSLAM_baseline
-from Baselines.baseline_mast3rslam import MAST3RSLAM_baseline_dev
-from Baselines.baseline_dpvo import DPVO_baseline
-from Baselines.baseline_dpvo import DPVO_baseline_dev
-from Baselines.baseline_monogs import MONOGS_baseline
-from Baselines.baseline_monogs import MONOGS_baseline_dev
-from Baselines.baseline_orbslam2 import ORBSLAM2_baseline
-from Baselines.baseline_orbslam2 import ORBSLAM2_baseline_dev
-from Baselines.baseline_colmap import COLMAP_baseline
-from Baselines.baseline_glomap import GLOMAP_baseline
-from Baselines.baseline_gensfm import GENSFM_baseline_dev
-from Baselines.baseline_mast3r import MAST3R_baseline_dev
-from Baselines.baseline_vggt import VGGT_baseline_dev
+# from Baselines.baseline_droidslam import DROIDSLAM_baseline
+# from Baselines.baseline_droidslam import DROIDSLAM_baseline_dev
+# from Baselines.baseline_mast3rslam import MAST3RSLAM_baseline
+# from Baselines.baseline_mast3rslam import MAST3RSLAM_baseline_dev
+# from Baselines.baseline_dpvo import DPVO_baseline
+# from Baselines.baseline_dpvo import DPVO_baseline_dev
+# from Baselines.baseline_monogs import MONOGS_baseline
+# from Baselines.baseline_monogs import MONOGS_baseline_dev
+# from Baselines.baseline_orbslam2 import ORBSLAM2_baseline
+# from Baselines.baseline_orbslam2 import ORBSLAM2_baseline_dev
+# from Baselines.baseline_colmap import COLMAP_baseline
+# from Baselines.baseline_glomap import GLOMAP_baseline
+# from Baselines.baseline_gensfm import GENSFM_baseline_dev
+# from Baselines.baseline_mast3r import MAST3R_baseline_dev
+# from Baselines.baseline_vggt import VGGT_baseline_dev
 
 from Baselines.baseline_orbslam3 import ORBSLAM3_baseline
 from Baselines.baseline_orbslam3 import ORBSLAM3_baseline_dev
+from Baselines.baseline_okvis2 import OKVIS2_baseline
 from Baselines.baseline_okvis2 import OKVIS2_baseline_dev
 
 def get_baseline_switcher():
     return {
-        "droidslam": lambda: DROIDSLAM_baseline(),
-        "droidslam-dev": lambda: DROIDSLAM_baseline_dev(),
-        "mast3rslam": lambda: MAST3RSLAM_baseline(),
-        "mast3rslam-dev": lambda: MAST3RSLAM_baseline_dev(),
-        "dpvo": lambda: DPVO_baseline(),
-        "dpvo-dev": lambda: DPVO_baseline_dev(),
-        "monogs": lambda: MONOGS_baseline(),
-        "monogs-dev": lambda: MONOGS_baseline_dev(),
-        "orbslam2": lambda: ORBSLAM2_baseline(),
-        "orbslam2-dev": lambda: ORBSLAM2_baseline_dev(),  
-        "colmap": lambda: COLMAP_baseline(),
-        "glomap": lambda: GLOMAP_baseline(),
-        "gensfm-dev": lambda: GENSFM_baseline_dev(),
-        "mast3r-dev": lambda: MAST3R_baseline_dev(),
-        "vggt-dev": lambda: VGGT_baseline_dev(),
+        # "droidslam": lambda: DROIDSLAM_baseline(),
+        # "droidslam-dev": lambda: DROIDSLAM_baseline_dev(),
+        # "mast3rslam": lambda: MAST3RSLAM_baseline(),
+        # "mast3rslam-dev": lambda: MAST3RSLAM_baseline_dev(),
+        # "dpvo": lambda: DPVO_baseline(),
+        # "dpvo-dev": lambda: DPVO_baseline_dev(),
+        # "monogs": lambda: MONOGS_baseline(),
+        # "monogs-dev": lambda: MONOGS_baseline_dev(),
+        # "orbslam2": lambda: ORBSLAM2_baseline(),
+        # "orbslam2-dev": lambda: ORBSLAM2_baseline_dev(),  
+        # "colmap": lambda: COLMAP_baseline(),
+        # "glomap": lambda: GLOMAP_baseline(),
+        # "gensfm-dev": lambda: GENSFM_baseline_dev(),
+        # "mast3r-dev": lambda: MAST3R_baseline_dev(),
+        # "vggt-dev": lambda: VGGT_baseline_dev(),
         "orbslam3": lambda: ORBSLAM3_baseline(),
         "orbslam3-dev": lambda: ORBSLAM3_baseline_dev(),
+        "okvis2": lambda: OKVIS2_baseline(),
         "okvis2-dev": lambda: OKVIS2_baseline_dev(),
     }
 
