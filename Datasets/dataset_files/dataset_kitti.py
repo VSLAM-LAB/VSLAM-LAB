953b27c88ed3b497b0f91b5c776badb4e31f6861
alejandrofontan <fontanvillacampa@gmail.com>
2025-12-30 18:10:22 +1000

    refactor: kitti dataset

--- Datasets/dataset_files/dataset_kitti.py
+++ Datasets/dataset_files/dataset_kitti.py
@@ -1,21 +1,20 @@
+
 from __future__ import annotations
 
-from pathlib import Path
-from typing import Final, Iterable
-from urllib.parse import urljoin
 import os
-import shutil
-
 import csv
 import yaml
-
+import shutil
 import numpy as np
+from typing import  Any
+from pathlib import Path
 from scipy.spatial.transform import Rotation as R
 
 from Datasets.DatasetVSLAMLab import DatasetVSLAMLab
 from utilities import downloadFile, decompressFile
 from path_constants import Retention, BENCHMARK_RETENTION, VSLAMLAB_BENCHMARK
-from Datasets.DatasetIssues import _get_dataset_issue
+from Datasets.DatasetVSLAMLab_issues import _get_dataset_issue
+
 
 class KITTI_dataset(DatasetVSLAMLab):
     """KITTI dataset helper for VSLAM-LAB benchmark."""
@@ -47,12 +46,12 @@ class KITTI_dataset(DatasetVSLAMLab):
         decompressed_folder = self.dataset_path / decompressed_name
 
         # Download the compressed file
-        if not os.path.exists(compressed_file):
+        if not compressed_file.exists():
             downloadFile(download_url, self.dataset_path)
             downloadFile(self.url_download_root_gt, self.dataset_path)
 
         # Decompress the file
-        if not os.path.exists(decompressed_folder):
+        if not decompressed_folder.exists():
             decompressFile(compressed_file, self.dataset_path)
             decompressFile(self.dataset_path / 'data_odometry_poses.zip', self.dataset_path)
 
@@ -60,11 +59,11 @@ class KITTI_dataset(DatasetVSLAMLab):
         sequence_path = self.dataset_path / sequence_name
         for rgb_i, image in zip(['rgb_0', 'rgb_1'], ['image_0', 'image_1']):    
             rgb_path = sequence_path / rgb_i
-            if not os.path.exists(rgb_path):
+            if not rgb_path.exists():
                 os.makedirs(rgb_path)
 
             rgb_path_raw = self.dataset_path / 'dataset' / 'sequences' / sequence_name / image
-            if not os.path.exists(rgb_path_raw):
+            if not rgb_path_raw.exists():
                 return
 
             for png_file in os.listdir(rgb_path_raw):
@@ -89,16 +88,17 @@ class KITTI_dataset(DatasetVSLAMLab):
 
         # Collect and sort image filenames
         rgb_path = sequence_path / 'rgb_0'
-        rgb_files = [f for f in os.listdir(rgb_path) if os.path.isfile(rgb_path / f)]
+        rgb_files = [f for f in os.listdir(rgb_path) if (rgb_path / f).is_file()]
         rgb_files.sort()
 
         # Write CSV with header
         with open(rgb_csv, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(['ts_rgb0 (s)', 'path_rgb0', 'ts_rgb1 (s)', 'path_rgb1']) 	
+            writer.writerow(['ts_rgb_0 (ns)', 'path_rgb_0', 'ts_rgb_1 (ns)', 'path_rgb_1']) 	
             for t, fname in zip(times, rgb_files):  # pairs safely to the shorter list
-                writer.writerow([f"{t:.6f}", f"rgb_0/{fname}", f"{t:.6f}", f"rgb_1/{fname}"])
-
+                t_ns = int(float(t) * 1e9)
+                writer.writerow([t_ns, f"rgb_0/{fname}", t_ns, f"rgb_1/{fname}"])
+        
     def create_calibration_yaml(self, sequence_name: str) -> None:
         calibration_txt = self.dataset_path / 'dataset' / 'sequences' / sequence_name / 'calib.txt'
 
@@ -108,17 +108,17 @@ class KITTI_dataset(DatasetVSLAMLab):
             calibration_1 = [value for value in file.readline().split()]
             fx_1, fy_1, cx_1, cy_1 = calibration_1[1], calibration_1[6], calibration_1[3], calibration_1[7]
 
-        rgb0 = {"cam_name": "rgb0", "cam_type": "rgb",
+        rgb0: dict[str, Any] = {"cam_name": "rgb_0", "cam_type": "gray",
                 "cam_model": "pinhole", "focal_length": [fx_0, fy_0], "principal_point": [cx_0, cy_0],
                 "fps": self.rgb_hz,
-                "T_SC": np.eye(4)}
+                "T_BS": np.eye(4)}
         
-        T_SC_1 = np.eye(4)
-        T_SC_1[0, 3] = float(calibration_1[4]) / float(fx_0)
-        rgb1 = {"cam_name": "rgb1", "cam_type": "rgb",
+        T_BS_1 = np.eye(4)
+        T_BS_1[0, 3] = -float(calibration_1[4]) / float(fx_0)
+        rgb1: dict[str, Any] = {"cam_name": "rgb_1", "cam_type": "gray",
                 "cam_model": "pinhole", "focal_length": [fx_1, fy_1], "principal_point": [cx_1, cy_1],
                 "fps": self.rgb_hz,
-                "T_SC": T_SC_1}
+                "T_BS": T_BS_1}
         self.write_calibration_yaml(sequence_name=sequence_name, rgb=[rgb0, rgb1])
     
     def create_groundtruth_csv(self, sequence_name: str) -> None:
@@ -143,7 +143,7 @@ class KITTI_dataset(DatasetVSLAMLab):
         poses_txt = self.dataset_path / 'dataset' / 'poses' / (sequence_name + '.txt')
         with open(poses_txt, 'r') as src, open(out_csv, 'w', newline='') as dst:
             writer = csv.writer(dst)
-            writer.writerow(['ts', 'tx', 'ty', 'tz', 'qx', 'qy', 'qz', 'qw'])  # header
+            writer.writerow(['ts (ns)', 'tx (m)', 'ty (m)', 'tz (m)', 'qx', 'qy', 'qz', 'qw'])
 
             for idx, line in enumerate(src):
                 if idx >= len(times):
@@ -156,8 +156,8 @@ class KITTI_dataset(DatasetVSLAMLab):
                 tx, ty, tz = vals[3], vals[7], vals[11]
                 qx, qy, qz, qw = R.from_matrix(Rm).as_quat()  # [x, y, z, w]
                 ts = times[idx]
-
-                writer.writerow([f"{ts:.6f}", tx, ty, tz, qx, qy, qz, qw])
+                ts_ns = int(float(ts)*1e9)
+                writer.writerow([ts_ns, tx, ty, tz, qx, qy, qz, qw])
 
     def get_download_issues(self, _):
         return [_get_dataset_issue(issue_id="complete_dataset", dataset_name=self.dataset_name, size_gb=23.2)]
